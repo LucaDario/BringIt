b751d8153cb29520cb42c540cd3d729c51c4ecf8
Stefano Lia <liastefano24@gmail.com>
2017-04-22 16:00:33 +0200

    Share: aggiunta documentazione

--- client/view/list/ShareListWithGroup/presenter/ShareWithGroupViewPresenter.js
+++ client/view/list/ShareListWithGroup/presenter/ShareWithGroupViewPresenter.js
@@ -6,9 +6,7 @@
  */
 import {ChatSource} from "../../../../chat/ChatSource";
 import {container, singleton, inject} from 'dependency-injection-es6';
-import {ChooseEventEmitter} from '../../../../event/ChooseEventEmitter';
 import {ShowPopupUseCase} from '../../../../usecase/ShowPopupUseCase';
-import {ShareWithGroupViewImpl} from "../view/ShareWithGroupViewImpl"
 
 export class ShareWithGroupViewPresenter{
 
@@ -19,63 +17,54 @@ export class ShareWithGroupViewPresenter{
     constructor(view){
         this._view = view;
         this._chat = container.resolve(ChatSource);
-        this._eventEmitter = container.resolve(ChooseEventEmitter);
-        this._eventEmitter.on('chooseEvent', (group,json) => {
-            console.log(json);
-
-
-            //send the message to the selected group
-            for(let i=0; i<group.length; i++) {
-                this._chat.sendMessageToChatWithJson(group[i], json);
-            }
-
-            //show the popup which contains the list of a member
-            for(let i=0; i<group.length; i++) {
-                console.log(group[i]);
-
-                //find the room's id
-                Meteor.call('getRoomIdByNameOrId',group[i], function (error1,result1) {
-                    if(result1) {
-                        //find the room's user
-                        Meteor.call('getUsersOfRoom', result1, true, function (error2, result2) {
-                            if (result2) {
-                                let show = container.resolve(ShowPopupUseCase);
-
-                                //html of the popup
-                                let html = '<h3 style="color: #FFFFFF">Selezione il membro a cui dare i permessi</h3>' +
-                                    '<select id="sites" name="sites[]" class="form-control" multiple="multiple">';
-                                for(let i=0; i<result2.records.length; i++){
-                                    html = html + '<option data-tokens="'+result2.records[i]+'">'
-                                        +result2.records[i]+'</option>';
-                                }
-                                html = html + '</select>';
-
-                                //callback that will be executed after popup's closing
-                                let f = function () {
-                                    let selected = $('#sites').val(); //get user's choice
-                                    console.log(json);
-                                    for(let i=0; i<selected.length;i++) {
-                                        Meteor.subscribe('sendPermissionsContact', json.listData.listData._id, selected);
-                                    }
-                                }
-
-                                //show the popup
-                                show.showPopup(html,f);
-                            }
-                            else {
-                                console.log(error2);
-                            }
-                        });
-                    }
-                    else{
-                        console.log(error1);
-                    }
-                });
-            }
-        });
-
     }
-    openShareWithGroupView(list, groupId){
-        this._chat.sendMessageToChatWithJson(groupId,list);
+    openShareWithGroupView(group,json){
+        //send the message to the selected group
+        for(let i=0; i<group.length; i++) {
+            this._chat.sendMessageToChatWithJson(group[i], json);
+        }
+
+        //show the popup which contains the list of the members of the group
+        for(let i=0; i<group.length; i++) {
+            //Find the room's id
+            Meteor.call('getRoomIdByNameOrId',group[i], function (error1,result1) {
+                if(result1) {
+
+                    //find the room's user
+                    Meteor.call('getUsersOfRoom', result1, true, function (error2, result2) {
+                        if (result2) {
+                            let show = container.resolve(ShowPopupUseCase);
+
+                            //html of the popup
+                            let html = '<h3 style="color: #FFFFFF">Selezione il membro a cui dare i permessi</h3>' +
+                                '<select id="sites" name="sites[]" class="form-control" multiple="multiple">';
+                            for(let i=0; i<result2.records.length; i++){
+                                html = html + '<option data-tokens="'+result2.records[i]+'">'
+                                    +result2.records[i]+'</option>';
+                            }
+                            html = html + '</select>';
+
+                            //callback that will be executed after popup's closing
+                            let f = function () {
+                                let selected = $('#sites').val(); //get user's choice
+                                console.log(json);
+                                for(let i=0; i<selected.length;i++) {
+                                    Meteor.subscribe('sendPermissionsContact', json.listData.listData._id, selected);
+                                }
+                            }
+
+                            //show the popup
+                            show.showPopup(html,f);
+                        }
+                        else {
+                            console.log(error2);
+                        }
+                    });
+                }
+                else{
+                    console.log(error1);
+                }
+            });
+        }
     }
 }
