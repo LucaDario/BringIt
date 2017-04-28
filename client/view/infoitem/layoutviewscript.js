a4a6360d75cde7b71f4a7738d565f8d5b3ed85ef
nicolo <nicolo.dovico@gmail.com>
2017-04-28 19:43:52 +0200

    aggiunto script per visualizzare dettagli ulteriori del item

---
+++ client/view/infoitem/layoutviewscript.js
@@ -0,0 +1,32 @@
+
+import {container, singleton, inject} from 'dependency-injection-es6';
+
+import {showlayoutadd} from '../../../../../client/usecase/ShowPopupUseCase';
+import {ShowPopupUseCase} from '../../usecase/ShowPopupUseCase';
+
+
+/**
+ * This script set event when the button with  class '.btn' clicked, and the event
+ *  call InputListInfoViewImpl.onSaveClicked
+ *
+ */
+function showlayoutadd(layout)
+{
+    let showPop = container.resolve(ShowPopupUseCase);
+
+    let immage = layout.getItems()[1];
+    let description = layout.getItems()[4];
+    let name= layout.getItems()[0].getText();
+
+    let new_layout= new Monolith.layout.VerticalLayoutView;
+    immage.setHeight(4);
+    immage.setWidth(4);
+    new_layout.addItem(immage);
+    new_layout.addItem(description);
+
+    showPop.showPopup(name,new_layout.renderView());
+
+
+
+}
+export {showlayoutadd};
